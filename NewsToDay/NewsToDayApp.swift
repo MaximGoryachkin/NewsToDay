ee906e68b35ad05d9a523390ec6410acb199a8df
Maxim Goryachkin <maxgoryachkin.dev@gmail.com>
2024-03-17 17:42:19 +0300

    Initial Commit

---
+++ NewsToDay/NewsToDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsToDayApp.swift
+//  NewsToDay
+//
+//  Created by Максим Горячкин on 17.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsToDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
