51889808f390d76f3a3e4a6fc6870d70c29e77f6
Maxim Goryachkin <maxgoryachkin.dev@gmail.com>
2024-03-17 19:20:21 +0300

    Добавил файлы проекта. Первоначальная настроил проект.

---
+++ NewsToDay/Extensions/Color + ext.swift
@@ -0,0 +1,26 @@
+//
+//  Color + ext.swift
+//  NewsToDay
+//
+//  Created by Максим Горячкин on 17.03.2024.
+//
+
+import SwiftUI
+
+extension Color {
+    static let purpleDark = Color(red: 37/255, green: 54/255, blue: 167/255)
+    static let purplePrimary = Color(red: 71/255, green: 90/255, blue: 215/255)
+    static let purpleLight = Color(red: 138/255, green: 150/255, blue: 229/255)
+    static let purpleLighter = Color(red: 238/255, green: 240/255, blue: 251/255)
+    
+    static let blackDark = Color(red: 24/255, green: 36/255, blue: 47/255)
+    static let blackPrimary = Color(red: 51/255, green: 54/255, blue: 71/255)
+    static let blackLight = Color(red: 68/255, green: 72/255, blue: 95/255)
+    static let blackLighter = Color(red: 85/255, green: 90/255, blue: 119/255)
+    
+    static let graykDark = Color(red: 102/255, green: 108/255, blue: 142/255)
+    static let grayPrimary = Color(red: 124/255, green: 130/255, blue: 161/255)
+    static let grayLight = Color(red: 172/255, green: 175/255, blue: 195/255)
+    static let grayLighter = Color(red: 243/255, green: 244/255, blue: 246/255)
+}
+
---
+++ NewsToDay/Extensions/Font + ext.swift
@@ -0,0 +1,24 @@
+//
+//  Font + ext.swift
+//  NewsToDay
+//
+//  Created by Максим Горячкин on 17.03.2024.
+//
+
+import SwiftUI
+
+extension Font {
+    static let interRegular12 = Font.custom("Inter-Regular", size: 12)
+    static let interRegular14 = Font.custom("Inter-Regular", size: 14)
+    static let interRegular16 = Font.custom("Inter-Regular", size: 16)
+    
+    static let interMedium14 = Font.custom("Inter-Medium", size: 14)
+    static let interMedium16 = Font.custom("Inter-Medium", size: 16)
+    
+    static let interSemiBold12 = Font.custom("Inter-SemiBold", size: 12)
+    static let interSemiBold16 = Font.custom("Inter-SemiBold", size: 16)
+    static let interSemiBold20 = Font.custom("Inter-SemiBold", size: 20)
+    static let interSemiBold24 = Font.custom("Inter-SemiBold", size: 24)
+    
+    static let interBold16 = Font.custom("Inter-Bold", size: 16)
+}
