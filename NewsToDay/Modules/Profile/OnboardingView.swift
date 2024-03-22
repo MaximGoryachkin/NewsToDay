adf5426546500ff660c15d0851198ef2c501c678
Vanya <whitewolf3310@icloud.com>
2024-03-23 00:57:08 +0300

    add homework task

---
+++ NewsToDay/Modules/Profile/OnboardingView.swift
@@ -0,0 +1,19 @@
+//
+//  OnboardingView.swift
+//  Profile
+//
+//  Created by Ваня Науменко on 20.03.24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+       Text("OnboardingView")
+        
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
