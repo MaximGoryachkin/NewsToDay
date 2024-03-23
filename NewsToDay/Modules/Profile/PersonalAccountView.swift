4ee7186d4e17a5bc09ecb7b9560f259cd0e9ace0
KrasnovYuri <krasnov1yuri@gmail.com>
2024-03-23 19:32:49 +0300

    add

---
+++ NewsToDay/Modules/Profile/PersonalAccountView.swift
@@ -0,0 +1,67 @@
+//
+//  PersonalAccountView.swift
+//  Profile
+//
+//  Created by Ваня Науменко on 20.03.24.
+//
+import SwiftUI
+
+// MARK: - PersonalAccountView
+
+struct PersonalAccountView: View {
+    @Environment(\.dismiss) var dismiss
+    @State private var termsAndConditionsView = false
+    var body: some View {
+        if #available(iOS 16.0, *) {
+            NavigationStack {
+                VStack(alignment: .leading, spacing: 40) {
+                    HStack {
+                        ProfileHeaderView()
+                        AccountDetailsView()
+                    }
+                    
+                    NavLinkButton(
+                        label: "Language",
+                        destination: Language()
+                    ).overlay {
+                        Image(systemName: "chevron.right")
+                            .padding()
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                    Spacer()
+                    VStack(spacing: 30) {
+                        NavLinkButton(
+                            label: "Terms & Conditions",
+                            destination: TermsAndConditionsView()
+                        ).overlay {
+                            Image(systemName: "chevron.right")
+                                .padding()
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                        }
+                        NavLinkButton(
+                            label: "Sing Out",
+                            destination: OnboardingView()
+                        ).overlay {
+                            Image(systemName: "ipad.and.arrow.forward")
+                                .padding()
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                        }
+                    }
+                }
+                .padding()
+                .navigationTitle("Profile")
+                
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+}
+
+// MARK: - PersonalAccountView_Previews
+
+struct PersonalAccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonalAccountView()
+    }
+}
